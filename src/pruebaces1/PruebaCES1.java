f4d04c1fc335fc4ca544e2b4de832881d2929f5e
federal.50802@gmail.com <>
2022-04-28 18:42:39 -0500

    Prueba CES 1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaces1;
+
+/**
+ *
+ * @author feder
+ */
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GIT HUB IBARRA 2842022");
+    }
+    
+}
